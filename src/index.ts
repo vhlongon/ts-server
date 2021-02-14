c689da4e312c89858dea20eee0f3f821085113f7
victor <vhlongon@gmail.com>
2021-02-14 21:51:29 +0100

    install types and create a express app

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log('start');
+import express, { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (req: Request, res: Response) => {
+  res.send(`<div><h1>Hi there</h1></div>`);
+});
+
+app.listen(3000, () => {
+  console.log('listening on port 3000');
+});
